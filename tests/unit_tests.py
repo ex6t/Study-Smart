8e45bc13c24082aa6641bbcc4335a9b6dee057a3
Keith-760 <palmerkw@icloud.com>
2026-07-31 14:56:36 -0700

    Added 4 planner unit tests

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -3,7 +3,8 @@ from unittest.mock import Mock, patch
 
 from app.screens.login_screen import LoginView
 from app.screens.settings_screen import SettingsScreen
-
+from app.screens.planner_screen import PlannerScreen
+from app.screens.all_plans_screen import AllPlansScreen
 
 #TEST TO ENSURE A SUCCESSFUL LOGIN STARTS USER SESSION AND REDIRECTS THE USER
 class LoginScreenTests(unittest.TestCase):
@@ -41,7 +42,89 @@ class SettingsScreenTests(unittest.TestCase):
 
         #ASSERTION
         end_session_mock.assert_called_once_with(current_window)
+class PlannerScreensTest(unittest.TestCase):
+    @patch("app.screens.planner_screen.save_plan",return_value=(True, "Plan Saved"))
+    def test_create_plan(self, save_plan_mock):
 
+        planner = Mock()
+
+        planner.user_id = 1
+        planner.editing_plan_id = None
+
+        planner.plan_name_input.text.return_value = "Math"
+
+        planner.plan_text_box.toPlainText.return_value = "Study Chapter 5"
+
+        planner.styled_message = Mock()
+        planner.reset_plan_form = Mock()
+        planner.plan_updated = Mock()
+
+        PlannerScreen.handle_save_plan(planner)
+
+        save_plan_mock.assert_called_once_with(
+            1,
+            "Math",
+            "Study Chapter 5"
+        )
+
+        planner.reset_plan_form.assert_called_once()
+        planner.plan_updated.emit.assert_called_once()
+    
+    
+    @patch("app.screens.planner_screen.update_plan",return_value=True)
+    def test_update_plan(self, update_plan_mock):
+
+        planner = Mock()
+
+        planner.user_id = 1
+        planner.editing_plan_id = 25
+
+        planner.plan_name_input.text.return_value = "Updated Plan"
+        planner.plan_text_box.toPlainText.return_value = "New Content"
+
+        planner.styled_message = Mock()
+        planner.reset_plan_form = Mock()
+        planner.plan_updated = Mock()
+
+        PlannerScreen.handle_save_plan(planner)
+
+        update_plan_mock.assert_called_once_with(25,1,"Updated Plan","New Content")
+
+        planner.reset_plan_form.assert_called_once()
+        planner.plan_updated.emit.assert_called_once()
+
+class AllPlansScreenTests(unittest.TestCase):
+
+    @patch("app.screens.all_plans_screen.clear_all_plans",
+           return_value=(True, "Plans cleared"))
+    def test_clear_all_plans(self, clear_all_mock):
+
+        screen = Mock()
+        screen.user_id = 1
+        screen.refresh_plans = Mock()
+
+        AllPlansScreen.clear_all_plans(screen)
+
+        clear_all_mock.assert_called_once_with(1)
+        screen.refresh_plans.assert_called_once()
+
+
+    def test_delete_plan_signal(self):
+
+        screen = Mock()
+
+        screen.delete_plan_requested = Mock()
+        screen.delete_plan_requested.emit = Mock()
+
+        plan = {
+            "id": 1,
+            "title": "Math",
+            "content": "Study"
+        }
+
+        screen.delete_plan_requested.emit(plan)
+
+        screen.delete_plan_requested.emit.assert_called_once_with(plan)
 
 if __name__ == "__main__":
     unittest.main()
