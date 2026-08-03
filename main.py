04c89619239a08791c73a1083ffe38f8b6f66dd8
Jawsh <50093768+ex6t@users.noreply.github.com>
2026-08-02 19:09:44 -0700

    added comments

--- main.py
+++ main.py
@@ -8,23 +8,27 @@ from app.screens.frontpageFE import WelcomeWindow
 from app.database.database import create_users_table
 from app.database.notes import create_notes_table
 from app.database.planner_database import create_planner_table
+
+#traceback exception handling
 def excepthook(exc_type, exc_value, exc_tb):
     tb = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
     print("Uncaught exception:\n", tb)
 
 sys.excepthook = excepthook
-
+#init main window
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         button = QPushButton("hi")
         self.setFixedSize(QSize(300, 300))
         self.setCentralWidget(button)
+#database table functions
 create_users_table()
 create_notes_table()
 create_flashcard_table()
 create_decks_table()
 create_planner_table()
+#main window starter
 app = QApplication(sys.argv)
 window = WelcomeWindow()
 window.show()
